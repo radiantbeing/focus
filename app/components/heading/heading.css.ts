4c7d2310a36fbbab545a7a08d67a0bffb79d04c7
Inhwa Kim <radiantbeing99@gmail.com>
2024-06-27 21:26:06 +0900

    refactor: `Heading` 컴포넌트 제거

--- app/components/heading/heading.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-import { vars } from '~/global.css';
-
-export const heading = style({
-  marginTop: 20,
-  marginBottom: 20,
-  fontSize: vars.fontSize.lg,
-});
