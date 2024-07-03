62c1014efb67c5eec827c16fef9eff10e6923cc4
Inhwa Kim <radiantbeing99@gmail.com>
2024-07-03 20:40:51 +0900

    chore: 아이콘 및 매니페스트 추가
    * 모바일 기기에서의 심미성을 위해 모바일 아이콘은 선의 굵기, 밀도, 배경색에 차이가 있음

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import './global.css';
 
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
   Links,
   Meta,
@@ -14,6 +14,37 @@ import { Main } from './components/main';
 import { Navbar } from './components/navbar';
 
 export const meta: MetaFunction = () => [{ title: 'FOCUS' }];
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'apple-touch-icon-precomposed',
+      sizes: '180x180',
+      href: '/apple-touch-icon-precomposed.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+  ];
+};
 
 export const Layout = ({ children }: { children: React.ReactNode }) => (
   <html lang="ko">
