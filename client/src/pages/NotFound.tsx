9e041e5a22c34e541dd9270ec9b4bd46b20d1e43
김인화 <radiantbeing99@gmail.com>
2025-09-01 01:34:13 +0900

    NotFound 컴포넌트를 Error 컴포넌트 기반으로 리팩터링

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 
+import Error from "../components/Error";
+
 export default function NotFound(): React.JSX.Element {
-  return (
-    <>
-      <div className="mt-1 mb-2 flex items-baseline gap-x-1">
-        <h1 className="text-xl font-bold">Not Found</h1>
-      </div>
-      <article>
-        <p>잘못된 접근입니다.</p>
-      </article>
-    </>
-  );
+  return <Error text="잘못된 접근입니다." />;
 }
