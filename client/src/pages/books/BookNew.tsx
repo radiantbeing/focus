49e4e9062c2630dab8ff2c3fdf75c673ab77a32e
김인화 <radiantbeing99@gmail.com>
2025-08-31 22:48:46 +0900

    제어 컴포넌트를 비제어 컴포넌트로 변경

--- client/src/pages/books/BookNew.tsx
+++ client/src/pages/books/BookNew.tsx
@@ -1,53 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
-import type { NewBook } from "../../../../shared/types";
-
 import { NewBookSchema } from "../../../../shared/validations";
 import Submit from "../../components/Submit";
 import { createBook } from "../../services/book";
 
 export default function BookNew(): React.JSX.Element {
   const navigate = useNavigate();
-  const [inputs, setInputs] = React.useState<NewBook>({
-    author: "",
-    title: ""
-  });
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
+  async function handleSubmit(formData: FormData): Promise<void> {
+    const author = formData.get("author");
+    const title = formData.get("title");
 
-    try {
-      const validInputs = NewBookSchema.parse(inputs);
-
-      createBook(validInputs)
-        .then(function ({ id }) {
-          return navigate(`/books/${id.toString()}`);
-        })
-        .catch(function (error: unknown) {
-          if (import.meta.env.DEV) {
-            console.error(error);
-          }
-        });
-    } catch (error: unknown) {
-      if (import.meta.env.DEV) {
-        console.error(error);
-      }
+    if (typeof author !== "string" || typeof title !== "string") {
+      return;
     }
-  }
 
-  function handleAuthorChange(
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void {
-    setInputs({ ...inputs, author: event.currentTarget.value });
-  }
-
-  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    setInputs({ ...inputs, title: event.currentTarget.value });
+    const inputs = NewBookSchema.parse({ author, title });
+    const createdBook = await createBook(inputs);
+    await navigate(`/books/${createdBook.id.toString()}`);
   }
 
   return (
-    <form method="post" onSubmit={handleSubmit}>
+    <form action={handleSubmit}>
       <div className="mt-1 mb-4 flex items-center justify-between">
         <div className="flex items-baseline gap-x-1">
           <h1 className="text-xl font-bold">도서 추가</h1>
@@ -60,18 +35,16 @@ export default function BookNew(): React.JSX.Element {
             <div className="mb-2 font-bold">도서명</div>
             <input
               className="block w-full border border-gray-300 px-1.5 py-1"
-              onChange={handleTitleChange}
+              name="title"
               required
-              value={inputs.title}
             />
           </label>
           <label className="block">
             <div className="mb-2 font-bold">저자</div>
             <input
               className="block w-full border border-gray-300 px-1.5 py-1"
-              onChange={handleAuthorChange}
+              name="author"
               required
-              value={inputs.author}
             />
           </label>
         </div>
