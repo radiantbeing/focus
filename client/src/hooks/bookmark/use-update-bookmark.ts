975d40f56ec6b61d18774c65a0521a30d1fd5f50
김인화 <radiantbeing99@gmail.com>
2025-09-04 18:59:24 +0900

    [client] useUpdateBookmark 훅 구현 및 적용

---
+++ client/src/hooks/bookmark/use-update-bookmark.ts
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router";
+
+import type { BookmarkId } from "../../../../shared/types";
+
+import { NewBookmarkSchema } from "../../../../shared/validations";
+import { updateBookmark } from "../../services/bookmark";
+
+interface UseUpdateBookmarkReturn {
+  handleUpdate: (formData: FormData) => Promise<void>;
+}
+
+export default function useUpdateBookmark(
+  id: BookmarkId | null
+): UseUpdateBookmarkReturn {
+  const navigate = useNavigate();
+
+  async function handleUpdate(formData: FormData): Promise<void> {
+    if (id === null) {
+      return;
+    }
+
+    const bookId = formData.get("bookId");
+    const page = formData.get("page");
+    const summary = formData.get("summary");
+
+    const inputs = NewBookmarkSchema.parse({
+      bookId: typeof bookId === "string" ? parseInt(bookId) : bookId,
+      page: typeof page === "string" ? parseInt(page) : page,
+      summary
+    });
+
+    const updatedBookmark = await updateBookmark(id, inputs);
+    await navigate(`/bookmarks/${updatedBookmark.id.toString()}`);
+  }
+
+  return { handleUpdate };
+}
--- client/src/pages/bookmarks/BookmarkUpdate.tsx
+++ client/src/pages/bookmarks/BookmarkUpdate.tsx
@@ -2,105 +2,54 @@ import { Undo2 } from "lucide-react";
 import React from "react";
 import { useNavigate, useParams } from "react-router";
 
-import type { Book, Bookmark } from "../../../../shared/types";
-
-import { NewBookmarkSchema } from "../../../../shared/validations";
+import Error from "../../components/Error";
 import IconButton from "../../components/IconButton";
+import Loading from "../../components/Loading";
 import Submit from "../../components/Submit";
-import { listBooks } from "../../services/book";
-import { getBookmark, updateBookmark } from "../../services/bookmark";
-import NotFoundPage from "../NotFound";
+import useBooks from "../../hooks/book/use-books";
+import useBookmark from "../../hooks/bookmark/use-bookmark";
+import useUpdateBookmark from "../../hooks/bookmark/use-update-bookmark";
 
 export default function BookmarkUpdate(): React.JSX.Element {
   const navigate = useNavigate();
   const { bookmarkId } = useParams();
 
-  const [books, setBooks] = React.useState<Book[] | null>(null);
-  const [bookmark, setBookmark] = React.useState<Bookmark | null>(null);
-
-  React.useEffect(
-    function () {
-      if (bookmarkId === undefined) {
-        return;
-      }
-
-      let ignore = false;
-
-      listBooks()
-        .then(function (books) {
-          if (!ignore) {
-            setBooks(books);
-          }
-        })
-        .catch(function () {
-          if (!ignore) {
-            setBooks(null);
-          }
-        });
-
-      getBookmark(parseInt(bookmarkId))
-        .then(function (bookmark) {
-          if (!ignore) {
-            setBookmark(bookmark);
-          }
-        })
-        .catch(function () {
-          {
-            if (!ignore) {
-              setBookmark(null);
-            }
-          }
-        });
-
-      return function (): void {
-        ignore = true;
-      };
-    },
-    [bookmarkId]
+  const { books, error: booksError, loading: booksLoading } = useBooks();
+  const {
+    bookmark,
+    error: bookmarkError,
+    loading: bookmarkLoading
+  } = useBookmark(bookmarkId === undefined ? null : parseInt(bookmarkId));
+  const { handleUpdate } = useUpdateBookmark(
+    bookmarkId === undefined ? null : parseInt(bookmarkId)
   );
 
   async function handleUndoButtonClick(): Promise<void> {
     await navigate(-1);
   }
 
-  async function handleSubmit(formData: FormData): Promise<void> {
-    if (bookmarkId === undefined) {
-      return;
-    }
-
-    const bookId = formData.get("bookId");
-    const page = formData.get("page");
-    const summary = formData.get("summary");
-
-    if (
-      typeof bookId !== "string" ||
-      typeof page !== "string" ||
-      typeof summary !== "string"
-    ) {
-      return;
-    }
-
-    const inputs = NewBookmarkSchema.parse({
-      bookId: parseInt(bookId),
-      page: parseInt(page),
-      summary
-    });
-
-    const updatedBookmark = await updateBookmark(parseInt(bookmarkId), inputs);
-    setBookmark(updatedBookmark);
-    await navigate(`/bookmarks/${updatedBookmark.id.toString()}`);
+  if (booksError !== null) {
+    return <Error text={`[${booksError.name}] ${booksError.message}`} />;
   }
 
-  if (bookmarkId === undefined) {
-    return <NotFoundPage />;
+  if (bookmarkError !== null) {
+    return <Error text={`[${bookmarkError.name}] ${bookmarkError.message}`} />;
   }
 
-  if (books === null || bookmark === null) {
-    return <div>로딩 중...</div>;
+  if (booksLoading) {
+    return <Loading text="도서 목록을 가져오는 중입니다." />;
+  }
+
+  if (bookmarkLoading) {
+    return <Loading text="책갈피 정보를 가져오는 중입니다." />;
+  }
+
+  if (bookmark === null) {
+    return <Error text="책갈피를 찾을 수 없습니다." />;
   }
 
   return (
-    <form action={handleSubmit}>
+    <form action={handleUpdate}>
       <div className="mt-1 mb-4 flex items-center justify-between">
         <div className="flex items-baseline gap-x-1">
           <h1 className="text-xl font-bold">책갈피 상세</h1>
