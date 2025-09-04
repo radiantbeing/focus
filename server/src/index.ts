5c1174f7b9aa5d2e6e5666d7c2436b6d525975ea
김인화 <radiantbeing99@gmail.com>
2025-09-05 00:55:55 +0900

    [server] 모바일 환경 테스트를 위한 헤더 변경

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ const bookService = new BookService(bookRepository, bookmarkRepository);
 const bookmarkService = new BookmarkService(bookmarkRepository);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
