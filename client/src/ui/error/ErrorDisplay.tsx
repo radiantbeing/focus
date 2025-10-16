3ea9b0fd60d28ef8af7188ad73ef06aa4fcd1d32
김인화 <radiantbeing99@gmail.com>
2025-10-16 17:38:51 +0900

    [client] ErrorDisplay 컴포넌트가 Error 객체를 처리하도록 변경

--- client/src/ui/error/ErrorDisplay.tsx
+++ client/src/ui/error/ErrorDisplay.tsx
@@ -21,16 +21,18 @@ export default function ErrorDisplay({
     [error]
   );
 
-  let defaultMessage = "알 수 없는 오류가 발생했습니다.";
+  let fallbackMessage = "알 수 없는 오류가 발생했습니다.";
 
   if (error instanceof ZodError) {
-    defaultMessage = "데이터 검증 오류가 발생했습니다.";
+    fallbackMessage = "데이터 검증 오류가 발생했습니다.";
+  } else if (error instanceof Error) {
+    fallbackMessage = error.message;
   }
 
   return (
     <article className="fixed inset-0 flex h-full flex-col items-center justify-center gap-y-3">
       <TriangleAlert className="text-gray-600" size={48} />
-      <p>{message ?? defaultMessage}</p>
+      <p>{message ?? fallbackMessage}</p>
     </article>
   );
 }
